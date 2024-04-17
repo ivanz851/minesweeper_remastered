d6df5f64095100255799dc4acb75704960573b51
ivanz-thinkpad <Ivan.zakharov.ru@yandex.ru>
2024-04-17 15:49:17 +0300

    Improved game page design

--- app/src/main/java/com/ivanz851/minesweeper/GameActivity.kt
+++ app/src/main/java/com/ivanz851/minesweeper/GameActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
